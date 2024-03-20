d6606cb99ccaf5dcc5f533f381d639c32a44fde7
AcidMink <163882978+AcidMink@users.noreply.github.com>
2024-03-20 10:59:29 +0200

    Add files via upload

---
+++ expansion.cpp
@@ -0,0 +1,113 @@
+#include "headings.h"
+
+int random3;
+int random4;
+
+string to_lower(string unfinished) {
+  transform(unfinished.begin(), unfinished.end(), unfinished.begin(), [](unsigned char c) {return tolower(c);});
+  return unfinished;
+}
+
+
+
+int player1_start_combat(int random3, int random4) {
+  cout << player1_name << " action: ", cin >> player1_action;
+  player1_action = to_lower(player1_action);
+  cout << player2_name << " action: ", cin >> player2_action;
+  player2_action = to_lower(player2_action);
+
+  if ((player1_action == "attack")&&(player2_action == "attack")) {
+    cout << player1_name << " defense: " << random3 << endl;
+    if (random3 < player1_def) {
+      cout << player1_name << " blockes attack!" << endl;
+    }
+    else {
+      player1_hp = player1_hp - player2_dmg;
+      cout << player1_name << " takes " << player2_dmg << " dmg" << endl;
+    }
+    cout << player2_name << " defense: " << random4 << endl;
+    if (random4 < player2_def) {
+      cout << player2_name << " blockes attack!" << endl;
+    }
+    else {
+      player2_hp = player2_hp - player1_dmg;
+      cout << player2_name << " takes " << player1_dmg << " dmg" << endl;
+    }
+  }
+  else if ((player1_action == "attack")&&(player2_action != "attack")) {
+    cout << player2_name << " fails to attack!" << endl;
+    cout << player2_name << " defense: " << random4 << endl;
+    if (random4 < player2_def) {
+      cout << player2_name << " blockes attack!" << endl;
+    }
+    else {
+      player2_hp = player2_hp - player1_dmg;
+      cout << player2_name << " takes " << player1_dmg << " dmg" << endl;
+    }
+  }
+  else if ((player1_action != "attack")&&(player2_action == "attack")) {
+    cout << player1_name << " fails to attack!" << endl;
+    cout << player1_name << " defense: " << random3 << endl;
+    if (random3 < player1_def) {
+      cout << player1_name << " blockes attack!" << endl;
+    }
+    else {
+      player1_hp = player1_hp - player2_dmg;
+      cout << player1_name << " takes " << player2_dmg << " dmg" << endl;
+    }
+  }
+  return 0;
+}
+int player2_start_combat(int random3, int random4) {  
+  cout << player2_name << " action: ", cin >> player2_action;
+  player2_action = to_lower(player2_action);
+  cout << player1_name << " action: ", cin >> player1_action;
+  player1_action = to_lower(player1_action);
+
+  srand (time(NULL));
+  random3 = rand() % 100 + 1;   //Player1 def chance random
+  srand (time(NULL));
+  random4 = rand() % 100 + 1;   //Player2 def chance random
+
+  if ((player1_action == "attack")&&(player2_action == "attack")) {
+    cout << player2_name << " defense: " << random4 << endl;
+    if (random4 < player2_def) {
+      cout << player2_name << " blockes attack!" << endl;
+    }
+    else {
+      player2_hp = player2_hp - player1_dmg;
+      cout << player2_name << " takes " << player1_dmg << " dmg" << endl;
+    }    
+    cout << player1_name << " defense: " << random3 << endl;
+    if (random3 < player1_def) {
+      cout << player1_name << " blockes attack!" << endl;
+    }
+    else {
+      player1_hp = player1_hp - player2_dmg;
+      cout << player1_name << " takes " << player2_dmg << " dmg" << endl;
+    }
+  }
+  else if ((player1_action == "attack")&&(player2_action != "attack")) {
+    cout << player2_name << " fails to attack!" << endl;
+    cout << player2_name << " defense: " << random4 << endl;
+    if (random4 < player2_def) {
+      cout << player2_name << " blockes attack!" << endl;
+    }
+    else {
+      player2_hp = player2_hp - player1_dmg;
+      cout << player2_name << " takes " << player1_dmg << " dmg" << endl;
+    }
+  }
+  else if ((player1_action != "attack")&&(player2_action == "attack")) {
+    cout << player1_name << " fails to attack!" << endl;
+    cout << player1_name << " defense: " << random3 << endl;
+    if (random3 < player1_def) {
+      cout << player1_name << " blockes attack!" << endl;
+    }
+    else {
+      player1_hp = player1_hp - player2_dmg;
+      cout << player1_name << " takes " << player2_dmg << " dmg" << endl;
+    }
+  }
+  return 0;
+}
---
+++ headings.h
@@ -0,0 +1,38 @@
+extern string player1_action;
+extern string player2_action;
+
+extern string player1_name;
+extern string player2_name;
+
+extern string player1_character;
+extern int player1_hp;
+extern int player1_hpcap;
+extern int player1_dmg;
+extern float player1_crit_chance;
+extern int player1_def;
+
+extern string player2_character;
+extern int player2_hp;
+extern int player2_hpcap;
+extern int player2_dmg;
+extern float player2_crit_chance;
+extern int player2_def;
+
+
+extern int random1;
+extern int random2;
+
+extern int assassin_hp;
+extern int assassin_dmg;
+extern float assassin_crit_chance;
+extern int draw_init_assassin;
+
+extern int knight_hp;
+extern int knight_dmg;
+extern float knight_crit_chance;
+extern int draw_init_knight;
+
+extern int demon_hp;
+extern int demon_dmg;
+extern float demon_crit_chance;
+extern int draw_init_demon;
