4e6da836740b7778c2a8dc072c026b563fda4b86
AcidMink <163882978+AcidMink@users.noreply.github.com>
2024-04-03 11:17:50 +0300

    shop update 2

--- main.cpp
+++ main.cpp
@@ -241,9 +241,10 @@ int main() {    //PvP TURN-BASED STRATEGY GAME
 
   while(true) {
     combat_phase();
+
+    upgrade_phase();
     player1_hp = player1_hpcap;
     player2_hp = player2_hpcap;
-    upgrade_phase();
   }
 
   cout << "END!";
